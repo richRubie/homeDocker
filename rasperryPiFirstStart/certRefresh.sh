19019ef1c46eca4baab508897a27b69024b2752b
Rich <rich.rubie@gmail.com>
2023-02-05 14:37:54 +0000

    move to rubie-todd.uk domain

--- rasperryPiFirstStart/certRefresh.sh
+++ rasperryPiFirstStart/certRefresh.sh
@@ -1,7 +1,7 @@
-sudo cp -RL ~/homeDocker/certbot/volumes/etc/live/redfoxfactory.co.uk ~/certStaging
+sudo cp -RL ~/homeDocker/certbot/volumes/etc/live/rubie-todd.uk ~/certStaging
 sudo chown -R pi:pi ~/certStaging/
-mv ~/certStaging/redfoxfactory.co.uk/* ~/certStaging/
-rmdir ~/certStaging/redfoxfactory.co.uk
+mv ~/certStaging/rubie-todd.uk/* ~/certStaging/
+rmdir ~/certStaging/rubie-todd.uk
 cat ~/certStaging/privkey.pem ~/certStaging/cert.pem > ~/certStaging/combined.pem
 mkdir ~/homeDocker/secrets/certArchive/$(date +"%Y-%m-%d")
 mv ~/homeDocker/secrets/cert/* ~/homeDocker/secrets/certArchive/$(date +"%Y-%m-%d")
