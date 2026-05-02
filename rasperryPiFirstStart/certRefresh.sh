d6528f66b143499699a20f9ab54ab6f6af811ba7
richRubie <rich.rubie@gmail.com>
2026-05-02 15:10:42 +0100

    Update certRefresh.sh to overwrite archive files when the archive folder already exists

--- rasperryPiFirstStart/certRefresh.sh
+++ rasperryPiFirstStart/certRefresh.sh
@@ -1,8 +1,13 @@
 sudo cp -RL ~/homeDocker/certbot/volumes/etc/live/rubie-todd.uk ~/certStaging
 sudo chown -R pi:pi ~/certStaging/
-mv ~/certStaging/rubie-todd.uk/* ~/certStaging/
+
+mv -f ~/certStaging/rubie-todd.uk/* ~/certStaging/
 rmdir ~/certStaging/rubie-todd.uk
+
 cat ~/certStaging/privkey.pem ~/certStaging/cert.pem > ~/certStaging/combined.pem
-mkdir ~/homeDocker/secrets/certArchive/$(date +"%Y-%m-%d")
-mv ~/homeDocker/secrets/cert/* ~/homeDocker/secrets/certArchive/$(date +"%Y-%m-%d")
-mv ~/certStaging/* ~/homeDocker/secrets/cert
+
+ARCHIVE_DIR=~/homeDocker/secrets/certArchive/$(date +"%Y-%m-%d")
+mkdir -p "$ARCHIVE_DIR"
+
+mv -f ~/homeDocker/secrets/cert/* "$ARCHIVE_DIR"/ 2>/dev/null || true
+mv -f ~/certStaging/* ~/homeDocker/secrets/cert
