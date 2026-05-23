2fac3cc75e9031211d7787ec55f6cf36838b74e6
richRubie <rich.rubie@gmail.com>
2026-05-23 22:50:11 +0100

    certbot restart dependent containers after cert update

--- rasperryPiFirstStart/certRefresh.sh
+++ rasperryPiFirstStart/certRefresh.sh
@@ -20,3 +20,13 @@ cp -L "$SRC/chain.pem" "$DEST/chain.pem"
 # Generate combined files for Pi-hole/Unifi/HA
 cat "$SRC/privkey.pem" "$SRC/cert.pem" > "$DEST/combined.pem"
 cp "$DEST/combined.pem" "$DEST/pihole.pem"
+
+# Install curl (needed to talk to docker socket)
+apk add --no-cache curl
+
+# Restart containers to pick up new certs
+# Ensure these names match your 'container_name' in docker-compose files
+for container in unifi pihole home-assistant; do
+    echo "Restarting $container..."
+    curl --unix-socket /var/run/docker.sock -X POST "http://localhost/containers/$container/restart"
+done
