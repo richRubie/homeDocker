7e2fe649bbdc4ffde1fe1ba3424f1d9e532c27a5
richRubie <rich.rubie@gmail.com>
2026-07-29 20:18:53 +0100

    Update setup.sh

--- rasperryPiFirstStart/setup.sh
+++ rasperryPiFirstStart/setup.sh
@@ -3,17 +3,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-# docker
-sudo apt install docker.io
+# docker - https://docs.docker.com/engine/install/debian/
 
-sudo systemctl enable docker
-
-sudo systemctl start docker
-
-sudo usermod -aG docker pi
-
-# docker compose
-sudo apt install -y python3-pip libffi-dev
-sudo pip3 install docker-compose
 
 sudo reboot
